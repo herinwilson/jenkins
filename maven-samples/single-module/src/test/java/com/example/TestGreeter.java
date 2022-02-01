cd78f2bea41fa1782c35ce54c6205b763d73652e
herin <herinwilson1@gmail.com>
2022-02-01 08:55:14 +0000

    added test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -40,4 +40,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+      @Test
+  public void greetShouldIncludeMessage() {
+    String someone = "World, Thanks for working";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
