248b9cc9b0a53632073939fe7ab11d1adc8c6250
herin <herinwilson1@gmail.com>
2022-02-02 12:19:22 +0000

    groovy dsl edit5

--- jobdsl.groovy
+++ jobdsl.groovy
@@ -2,7 +2,7 @@ freeStyleJob('job created from coed dsl') {
     logRotator(-1, 10)
     description('job created from coed dsl')
     scm {
-        git("https://github.com/herinwilson/jenkins.git", master )
+        git("https://github.com/herinwilson/jenkins.git", 'master')
         }
     triggers {
         scm('* * * * *')
