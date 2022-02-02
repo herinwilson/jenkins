7006430c1de8ebce85296e8163089b9020759d28
jenkins <herin@gmail.com>
2022-02-02 14:16:19 +0000

    added new file

--- seedjobs/jobdsl.groovy
+++ seedjobs/jobdsl.groovy
@@ -2,7 +2,7 @@ freeStyleJob('job created from code dsl') {
     logRotator(-1, 10)
     description('job created from coed dsl')
     scm {
-        git("git@github.com:herinwilson/jenkins.git", '*/' 'master' )
+        git("git@github.com:herinwilson/jenkins.git", '*/master' )
     }
     triggers {
         scm('* * * * *')
