bc8bd3d09d03fbde9b8d0585fa4b2127c1e7b391
jenkins <herin@gmail.com>
2022-02-02 14:04:52 +0000

    created new groovy folder

---
+++ seedjobs/jobdsl.groovy
@@ -0,0 +1,17 @@
+freeStyleJob('job created from coed dsl') {
+    logRotator(-1, 10)
+    description('job created from coed dsl')
+    scm {
+        git("https://github.com/herinwilson/jenkins.git", 'master')
+        }
+    triggers {
+        scm('* * * * *')
+    }
+    steps {
+        maven ('clean package', 'maven-samples/single-module/pom.xml')  
+    }
+    publishers {
+        archiveArtifacts('**/*.jar')
+        }
+}
+
